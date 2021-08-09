5031ae368ee12183faa29bfab0847283db392e5e
luisbezerra <luis.bezerra@wlcsolucoes.com.br>
2021-08-09 16:42:39 -0300

    Reatoração das respostas

--- server.py
+++ server.py
@@ -1,43 +1,50 @@
+import os
+
 from http.server import BaseHTTPRequestHandler
+
 from routes.main import routes
-from pathlib import Path
+
+from response.templateHandler import TemplateHandler
+from response.badRequestHandler import BadRequestHandler
 
 
 class Server(BaseHTTPRequestHandler):
     def do_HEAD(self):
         return
 
-    def do_POST(self):
-        return
-
     def do_GET(self):
-        self.respond()
+        split_path = os.path.splitext(self.path)
+        request_extension = split_path[1]
 
-    def handle_http(self):
-        status = 200
-        content_type = "text/plain"
-        response_content = ""
-
-        if self.path in routes:
-            print(routes[self.path])
-            route_content = routes[self.path]['template']
-            filepath = Path("templates/{}".format(route_content))
-            if filepath.is_file():
-                content_type = "text/html"
-                response_content = open("templates/{}".format(route_content))
-                response_content = response_content.read()
+        if request_extension == "" or request_extension == ".html":
+            if self.path in routes:
+                handler = TemplateHandler()
+                handler.find(routes[self.path])
             else:
-                content_type = "text/plain"
-                response_content = "404 Not Found"
+                handler = BadRequestHandler()
+
         else:
-            content_type = "text/plain"
-            response_content = "404 Not Found"
+            handler = BadRequestHandler()
 
-        self.send_response(status)
-        self.send_header('Content-type', content_type)
+        self.respond({
+            'handler': handler
+        })
+
+    def handle_http(self, handler):
+        status_code = handler.getStatus()
+
+        self.send_response(status_code)
+
+        if status_code == 200:
+            content = handler.getContents()
+            self.send_header('Content-type', handler.getContentType())
+        else:
+            content = "404 Not Found"
+
         self.end_headers()
-        return bytes(response_content, "UTF-8")
 
-    def respond(self):
-        content = self.handle_http()
-        self.wfile.write(content)
+        return bytes(content, 'UTF-8')
+
+    def respond(self, opts):
+        response = self.handle_http(opts['handler'])
+        self.wfile.write(response)
